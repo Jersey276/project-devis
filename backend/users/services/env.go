a6d63c28b90030814dabb4e13bd74042bbb4806f
Jersey276 <tristan-lefevre@hotmail.fr>
2026-04-19 22:23:24 +0200

    feat(user): install env service

---
+++ backend/users/services/env.go
@@ -0,0 +1,24 @@
+package services
+
+import "os"
+
+type EnvKey string
+
+const (
+	Env                     EnvKey = "ENV"
+	PostgresUser            EnvKey = "POSTGRES_USER"
+	PostgresPassword        EnvKey = "POSTGRES_PASSWORD"
+	PostgresDefaultDatabase EnvKey = "POSTGRES_DB"
+	PostgresDatabaseAddress EnvKey = "POSTGRES_DB_ADDRESS"
+	PostgresDatabasePort    EnvKey = "POSTGRES_DB_PORT"
+	DBHost                  EnvKey = "DB_HOST"
+	DBPort                  EnvKey = "DB_PORT"
+	DBUser                  EnvKey = "DB_USER"
+	DBPassword              EnvKey = "DB_PASSWORD"
+	DBPasswordFile          EnvKey = "DB_PASSWORD_FILE"
+	DBName                  EnvKey = "DB_NAME"
+)
+
+func (key EnvKey) GetValue() string {
+	return os.Getenv(string(key))
+}
