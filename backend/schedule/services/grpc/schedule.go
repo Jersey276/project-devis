c795ed36b90e099fb7a3f1009c4b0b9270877375
Jersey276 <tristan-lefevre@hotmail.fr>
2026-05-30 23:16:47 +0200

    feat(schedule): create migration + schedule base service

---
+++ backend/schedule/services/grpc/schedule.go
@@ -0,0 +1,52 @@
+package grpc
+
+import (
+	"context"
+
+	grpcpkg "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ScheduleServiceServer interface {
+	mustEmbedUnimplementedScheduleServiceServer()
+}
+
+type UnimplementedScheduleServiceServer struct{}
+
+func (UnimplementedScheduleServiceServer) mustEmbedUnimplementedScheduleServiceServer() {}
+
+func RegisterScheduleServiceServer(s grpcpkg.ServiceRegistrar, srv ScheduleServiceServer) {
+	s.RegisterService(&grpcpkg.ServiceDesc{
+		ServiceName: "schedule.ScheduleService",
+		HandlerType: (*ScheduleServiceServer)(nil),
+		Methods:     []grpcpkg.MethodDesc{},
+		Streams:     []grpcpkg.StreamDesc{},
+		Metadata:    "schedule.proto",
+	}, srv)
+}
+
+func UnimplementedError() error {
+	return status.Error(codes.Unimplemented, "method not implemented")
+}
+
+type GenericResponse struct {
+	Success bool
+	Code    int32
+}
+
+type CreateScheduleRequest struct {
+	UserId         string
+	QuoteId        string
+	Name           string
+	StartMonth     string
+	DurationMonths int32
+}
+
+type CreateScheduleResponse struct {
+	Success    bool
+	Code       int32
+	ScheduleId string
+}
+
+var _ context.Context
